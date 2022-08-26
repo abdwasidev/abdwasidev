0c048a9a40fe1b7403e4429880f30bf01094852c
ABDUL WASI <73805930+abdwasidev@users.noreply.github.com>
2022-08-26 19:40:01 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,30 +1,63 @@
-function skills() {
-    const skill_item = [
-        "C/C++", "HTML", "CSS", 
-        "BOOTSTRAP", "JAVASCRIPT", "PHP", 
-        "NODE JS"
+function openTabs(evt, ContentTabs) {
+    var i, tabcontent, buttontabs;
+
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+
+    buttontabs = document.getElementsByClassName("buttontabs");
+    for (i = 0; i < buttontabs.length; i++) {
+        buttontabs[i].className = buttontabs[i].className.replace(" active", "");
+    }
+
+    document.getElementById(ContentTabs).style.display = "block";
+    evt.currentTarget.className += " active";
+}
+
+document.getElementById("default").click();
+
+
+// BUTTON
+function tools1() {
+    const programming_item = [
+        "HTML", "CSS", "JAVASCRIPT"
     ];
 
     var print = "";
-    for (var i = 0; i < skill_item.length; i++) {
-        print += "<button>" + skill_item[i] + "</button>";
+    for (var i = 0; i < programming_item.length; i++) {
+        print += "<button>" + programming_item[i] + "</button>";
     }
     return print;
 
 }
-document.getElementById('skills-item').innerHTML = skills();
+document.getElementById('tools1').innerHTML = tools1();
 
-function tools() {
-    const tools_item = [
-        "VISUAL STUDIO CODE", 
-        "FIGMA", "ADOBE XD"
+function tools2() {
+    const programming_item = [
+        "HTML", "CSS", "JAVASCRIPT"
     ];
 
     var print = "";
-    for (var i = 0; i < tools_item.length; i++) {
-        print += "<button>" + tools_item[i] + "</button>";
+    for (var i = 0; i < programming_item.length; i++) {
+        print += "<button>" + programming_item[i] + "</button>";
     }
     return print;
-}
 
-document.getElementById('tools-item').innerHTML = tools();
+}
+document.getElementById('tools2').innerHTML = tools2();
+
+function tools3() {
+    const programming_item = [
+        "HTML", "CSS", "JAVASCRIPT"
+    ];
+
+    var print = "";
+    for (var i = 0; i < programming_item.length; i++) {
+        print += "<button>" + programming_item[i] + "</button>";
+    }
+    return print;
+
+}
+document.getElementById('tools3').innerHTML = tools3();
+
